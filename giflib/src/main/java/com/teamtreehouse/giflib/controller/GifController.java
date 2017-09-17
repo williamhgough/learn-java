7ff2cd050a92938e0687ca293ed0d9417b3e2f6f
William Gough <williamhenrygough@gmail.com>
2017-09-17 20:58:26 +0100

    giflib

---
+++ giflib/src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -0,0 +1,28 @@
+package com.teamtreehouse.giflib.controller;
+
+import com.teamtreehouse.giflib.data.GifRepository;
+import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GifController {
+
+    @Autowired
+    private GifRepository gifRepository;
+
+    @RequestMapping("/")
+    public String listGifs() {
+        return "home";
+    }
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
+        modelMap.put("gif", gif);
+        return "gif-details";
+    }
+}
